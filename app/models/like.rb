4b3b0857e929f4960d8b04aa8a2248d5ff582888
Evans kofi Nyamekye <nyamekye2131@gmail.com>
2023-11-30 11:58:22 +0000

    Update like.rb

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,7 +3,9 @@ class Like < ApplicationRecord
   belongs_to :post
 
   after_save :update_likes_counter
+  
   private
+  
   def update_likes_counter
     post.update(likes_counter: post.likes.count)
   end

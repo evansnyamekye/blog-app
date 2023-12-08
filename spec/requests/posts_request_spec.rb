cafce676b77149a1828b5605b29935b768068634
Evans K. Nyamekye <nyamekye2131@gmail.com>
2023-12-08 00:05:20 +0000

    add request for testing of controller

--- spec/requests/posts_request_spec.rb
+++ spec/requests/posts_request_spec.rb
@@ -1,7 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  it 'performs an operation' do
-    expect(true).to be(true)
+  describe 'GET #index'
+    it 'return a success resopone' do
+      user = FactoryBot.create(:user)
+      FactoryBot.create(:post, author: user)
+      get user_posts_path(user)
+      expect(resopone).to have_http_status(:success)
+    end
+
+   it 'renders the index template' do 
+    user = FactoryBot.create(:user)
+    FactoryBot.create(:post, author: user)
+    get user_posts_path(user)
+    expect(resopone).to render_template(:index)
+   end
+
+   it 'includes correct placeholder text in the response body' do 
+    user = FactoryBot.create(:user)
+    FactoryBot.create(:post, author: user)
+    get user_posts_path(user)
+    expect(resopone.body).to include('Great Quotes')
+   end
+ end
+
+ describe 'GET #show' do
+  it 'returns a success responese' do
+    user = FactoryBot.create(:user)
+    post = FactoryBot.create(:post, author: user)
+    get user_posts_path(user, post)
+    expect(response).to have_http_status(success)
+  end 
+
+  it 'renders the show template' do 
+    user = FactoryBot.create(:user)
+    post = FactoryBot.create(:post, author: user)
+    get user_posts_path(user, post)
+    expect(resopone).to render_template(:show)
+  end
+
+  it 'includes correct placeholder text in the response body' do 
+    user = FactoryBot.create(:user)
+    post = FactoryBot.create(:post, author: user)
+    get user_posts_path(user, post)
+    expect(resopone.body).to include('Great Quotes')
+   end
   end
 end

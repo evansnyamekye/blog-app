59128015c25bc5743c529cc2b134fcac968975ab
Evans K. Nyamekye <nyamekye2131@gmail.com>
2023-12-08 00:16:44 +0000

    Update: clear linters error

--- spec/requests/posts_request_spec.rb
+++ spec/requests/posts_request_spec.rb
@@ -2,47 +2,47 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET #index'
-    it 'return a success resopone' do
-      user = FactoryBot.create(:user)
-      FactoryBot.create(:post, author: user)
-      get user_posts_path(user)
-      expect(resopone).to have_http_status(:success)
-    end
+  it 'return a success resopone' do
+    user = FactoryBot.create(:user)
+    FactoryBot.create(:post, author: user)
+    get user_posts_path(user)
+    expect(resopone).to have_http_status(:success)
+  end
 
-   it 'renders the index template' do 
+  it 'renders the index template' do
     user = FactoryBot.create(:user)
     FactoryBot.create(:post, author: user)
     get user_posts_path(user)
     expect(resopone).to render_template(:index)
-   end
+  end
 
-   it 'includes correct placeholder text in the response body' do 
+  it 'includes correct placeholder text in the response body' do
     user = FactoryBot.create(:user)
     FactoryBot.create(:post, author: user)
     get user_posts_path(user)
     expect(resopone.body).to include('Great Quotes')
-   end
- end
+  end
+end
 
- describe 'GET #show' do
+describe 'GET #show' do
   it 'returns a success responese' do
     user = FactoryBot.create(:user)
     post = FactoryBot.create(:post, author: user)
     get user_posts_path(user, post)
     expect(response).to have_http_status(success)
-  end 
+  end
 
-  it 'renders the show template' do 
+  it 'renders the show template' do
     user = FactoryBot.create(:user)
     post = FactoryBot.create(:post, author: user)
     get user_posts_path(user, post)
     expect(resopone).to render_template(:show)
   end
 
-  it 'includes correct placeholder text in the response body' do 
+  it 'includes correct placeholder text in the response body' do
     user = FactoryBot.create(:user)
     post = FactoryBot.create(:post, author: user)
     get user_posts_path(user, post)
     expect(resopone.body).to include('Great Quotes')
-   end
+  end
 end

1bdf6a0d712b85238bbb6b6d4125f1af20bf8f67
Evans K. Nyamekye <nyamekye2131@gmail.com>
2023-12-09 13:34:48 +0000

    Update: resolved issues on posts_spec.rb

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,25 +2,25 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET #index'
-  it 'return a success resopone' do
+  it 'return a success response' do
     user = FactoryBot.create(:user)
     FactoryBot.create(:post, author: user)
     get user_posts_path(user)
-    expect(resopone).to have_http_status(:success)
+    expect(response).to have_http_status(:success)
   end
 
   it 'renders the index template' do
     user = FactoryBot.create(:user)
     FactoryBot.create(:post, author: user)
     get user_posts_path(user)
-    expect(resopone).to render_template(:index)
+    expect(response).to render_template(:index)
   end
 
   it 'includes correct placeholder text in the response body' do
     user = FactoryBot.create(:user)
     FactoryBot.create(:post, author: user)
     get user_posts_path(user)
-    expect(resopone.body).to include('Great Quotes')
+    expect(response.body).to include('Great Quotes')
   end
 end
 
@@ -28,21 +28,21 @@ describe 'GET #show' do
   it 'returns a success responese' do
     user = FactoryBot.create(:user)
     post = FactoryBot.create(:post, author: user)
-    get user_posts_path(user, post)
-    expect(response).to have_http_status(success)
+    get user_post_path(user, post)
+    expect(response).to have_http_status(:success)
   end
 
   it 'renders the show template' do
     user = FactoryBot.create(:user)
     post = FactoryBot.create(:post, author: user)
     get user_posts_path(user, post)
-    expect(resopone).to render_template(:show)
+    expect(response).to render_template(:show)
   end
 
   it 'includes correct placeholder text in the response body' do
     user = FactoryBot.create(:user)
     post = FactoryBot.create(:post, author: user)
     get user_posts_path(user, post)
-    expect(resopone.body).to include('Great Quotes')
+    expect(response.body).to include('Great Quotes')
   end
 end

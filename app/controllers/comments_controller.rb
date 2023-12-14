42116babe227c68f78a428a58df3c25b34d0cccf
Evans kofi Nyamekye <nyamekye2131@gmail.com>
2023-12-14 14:00:11 +0000

    Update comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,4 @@
 class CommentsController < ApplicationController
-#before_action :set_user_and_post, only: %i[new create]
 
   def new
     store_referer
@@ -24,11 +23,6 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:text)
   end
-
-#   def set_user_and_post
-#     @user = User.find(params[:user_id])
-#     @post = @user.posts.find(params[:post_id])
-#end
 
   def store_referer
     session[:referer] = request.referer

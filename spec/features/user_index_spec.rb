2a15e8a812c3068e16605128567618f26475ec94
Evans K. Nyamekye <nyamekye2131@gmail.com>
2023-12-17 02:04:35 +0000

    Update: Fix linters by working on blocklength

--- spec/features/user_index_spec.rb
+++ spec/features/user_index_spec.rb
@@ -9,33 +9,28 @@ RSpec.feature 'User Index', type: :feature do
   end
 
   scenario 'displays correct number of users' do
-    visit users_path
     expect(page).to have_selector('.user', count: User.count)
   end
 
   scenario 'displays all users photo' do
-    visit users_path
     User.all.each do |_user|
       expect(page).to have_css("img[alt='User Photo']")
     end
   end
 
   scenario 'displays all users names' do
-    visit users_path
     User.all.each do |user|
       expect(page).to have_content(user.name)
     end
   end
 
   scenario 'displays all users posts count' do
-    visit users_path
     User.all.each do |user|
       expect(page).to have_content("Posts: #{user.posts.count}")
     end
   end
 
   scenario 'redirects to user show page when clicked' do
-    visit users_path
     User.all.each do |user|
       click_link user.name
       expect(current_path).to eq(user_path(user))

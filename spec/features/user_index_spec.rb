3c6b59d68a3e8f79b28dd2408d290ca37a07ceab
Nizamuddin4493 <nizamuddin4493@gmail.com>
2023-12-19 19:19:06 +0430

    User show specs done remove the errors

--- spec/features/user_index_spec.rb
+++ spec/features/user_index_spec.rb
@@ -10,30 +10,30 @@ RSpec.feature 'User Index', type: :feature do
 
   describe 'User index page' do
     it 'displays correct number of users' do
-    expect(page).to have_css('div.user_info', count: 5)
-  end
-
-  describe 'displays all users photo' do
-    user.each do |_user|
-      expect(page).to have_css("img[alt='User Photo']")
+      expect(page).to have_css('div.user_info', count: 5)
     end
-  end
 
-  describe 'displays all users names' do
-    user.each do |user|
-      expect(page).to have_content(user.name)
+    describe 'displays all users photo' do
+      user.each do |_user|
+        expect(page).to have_css("img[alt='User Photo']")
+      end
     end
-  end
 
-  describe 'displays all users posts count' do
-    user.each do |user|
-      expect(page).to have_content(user.posts.count)
+    describe 'displays all users names' do
+      user.each do |user|
+        expect(page).to have_content(user.name)
+      end
     end
-  end
 
-  #redirect to a new page when clicked
-  describe 'redirects to user show page when clicked' do
-   find_all('a', text: users.first.name)[0].click
+    describe 'displays all users posts count' do
+      user.each do |user|
+        expect(page).to have_content(user.posts.count)
+      end
+    end
+
+    # redirect to a new page when clicked
+    describe 'redirects to user show page when clicked' do
+      find_all('a', text: users.first.name)[0].click
       expect(page).to have_current_path(user_path(users.first))
     end
   end
